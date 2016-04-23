5bfeab95df7c0994388ad4d7a641de273be285f9
willchen108 <wchen108@uw.edu>
2016-04-23 14:46:21 -0700

    Updated

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # HiC-Analysis
-## All the codes related to Hi-C data analysis are here. 
-This is a test to see if it can sync
+All the codes related to Hi-C data analysis are here. 
---
+++ bed file processing/decouple_bed.py
@@ -0,0 +1,38 @@
+#Create by Will Chen @ 2016.04.11, borrow from sort_strandedness_bulk.py
+import os,sys,re
+#BED file of mapped reads
+fhi = open(sys.argv[1])
+
+#Just a list with different types of ligation junctions
+
+for line in fhi:
+	split = line.split() #split BED file
+	#Chr1 
+	fcoord1 = int(split[1]) 
+	rcoord1 = int(split[2])
+	#Chr2
+	fcoord2 = int(split[4])
+	rcoord2 = int(split[5]) 
+	#name
+	ID = split[6]
+	mapq = int(split[7]) # mapq
+	species1 = split[0] #ChrID of Mate 1
+	species2 = split[3] #ChrID of Mate 2
+	#get strands
+	s1 = split[8]
+	s2 = split[9]
+	if species1 == species2:
+		if int(fcoord1) > int(fcoord2):
+			strand1 = s2
+			strand2 = s1
+		else:
+			strand1 = s1
+			strand2 = s2
+	elif species1 >= species2:
+		strand1 = s2
+		strand2 = s1
+	else:
+		strand1 = s1
+		strand2 = s2
+	print "%s\t%s\t%s\t%s\t%s" % (species1, fcoord1, rcoord1, ID, strand1) 
+	print "%s\t%s\t%s\t%s\t%s" % (species2, fcoord2, rcoord2, ID, strand2)
