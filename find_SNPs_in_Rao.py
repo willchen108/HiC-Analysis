cb8793de6f5c7409225279a90eeb0e5f21907498
willchen108 <wchen108@uw.edu>
2016-04-24 22:35:48 -0700

    final

--- find_SNPs_in_Rao.py
+++ find_SNPs_in_Rao.py
@@ -8,13 +8,13 @@ readfile = open(sys.argv[1])
 SNPsfile = open(sys.arge[2])
 distance = int(sys.arge[3])
 for line in readfile:
-    chr1,x1,x2,chr2,y1,y2 = line.split()[0:6] 
-    for line2 in SNPsfile:
-    	chrid, snps = line2.split()[0:2]
-    	chrid = chrid[0]
-      	if chr1 == chrid:
-      		if snps in range(x1-distance,x2+distance):
-      			print "%s\t%s\t%s\t%s\t%s\t%s" % (chr1, x1, x2, y1, y2, snps) 
+	chr1,x1,x2,chr2,y1,y2 = line.split()[0:6] 
+	for line2 in SNPsfile:
+		chrid, snps = line2.split()[0:2]
+		chrid = chrid[0]
+		if chr1 == chrid:
+			if snps in range(x1-distance,x2+distance):
+				print "%s\t%s\t%s\t%s\t%s\t%s" % (chr1, x1, x2, y1, y2, snps) 
 			elif snps in range(y1-distance,y2+distance):
 				print "%s\t%s\t%s\t%s\t%s\t%s" % (chr1, y1, y2, x1, x2, snps) 
 
