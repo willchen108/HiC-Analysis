69caf2f40880b11e40ab60ca352253aa89c99851
willchen108 <wchen108@uw.edu>
2016-04-26 14:57:00 -0700

    up

--- SNPscap_to_loops.py
+++ SNPscap_to_loops.py
@@ -26,4 +26,4 @@ for line in bedfile:
 		for loc in loops[chr1]:
 			y1, y2 = loc[2:4]
 			if f1 in range(y1,y2) or r1 in range(y1,y2) or f2 in range(y1,y2) or r2 in range(y1,y2):
-				print "%s" % (read.rstrip('\n'))
+				print "%s" % (line.rstrip('\n'))
