a68acf83013528ecb1d1b8545a2cc6106315db2f
willchen108 <wchen108@uw.edu>
2016-04-26 21:46:11 -0700

    up

--- mapping_reads_to_loops.py
+++ mapping_reads_to_loops.py
@@ -1,15 +1,33 @@
 # This file is created by Will @ 2016.04.26
-# This code is used to find the reads in SNPs capture that fall into our 55 loops list.
-
+# This code is used to find the reads that fall into our 55 loops list.
+# /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/eQTL_SNPs_151228/Promoters/1/10847_1k.bed ~/data
 import os,sys,re
 
-bed = open(sys.argv[1])
-loops = open(sys.argv[2])
+bedfile = open(sys.argv[1])
+loopsfile = open(sys.argv[2])
 
-for read in bed:
-	chr1, f1, r1, chr2, f2, r2 = read.split()[0:6]
-	for loop in loops:
-		chrid, x1, x2, y1, y2 = loop.split()[0:5]
-		if chr1 == chrid:
-			if mid1 in range(y1,y2) or mid2 in range(y1,y2):
-				print "%s" % (read.rstrip('\n'))
+loops={}
+for loop in loopsfile:
+	chrid, x1, x2, y1, y2 = loop.split()[0:5]
+	if chrid in loops:
+		loops[chrid].append([int(x1), int(x2), int(y1), int(y2)])
+	else:
+		loops[chrid] = []
+		loops[chrid].append([int(x1), int(x2), int(y1), int(y2)])
+
+for line in bedfile:
+	chr1, f1, r1, chr2, f2, r2 = line.split()[0:6]
+	f1, r1, f2, r2 = int(f1), int(r1), int(f2), int(r2)
+	try:
+		int(chr1)
+	except ValueError:
+		continue
+	if chr1 in loops:
+		for loc in loops[chr1]:
+			y1, y2 = loc[2:4]
+			if f1 in range(y1,y2) or r1 in range(y1,y2):
+				if f2 in range(x1,x2) or r2 in range(x1,x2):
+					print "%s" % (line.rstrip('\n'))
+			elif f2 in range(y1,y2) or r2 in range(y1,y2):
+				if f1 in range(x1,x2) or r1 in range(x1,x2):
+					print "%s" % (line.rstrip('\n'))
---
+++ mapping_reads_to_loops_bulk.sh
@@ -0,0 +1,23 @@
+#Create by Will Chen @ 2016.04.11
+#require 2 cores 12G memory.
+#USAGE: bed_partition_bulk.sh <workdirectory> example /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/eQTL_SNPs_151228/Promoters/
+#VARS
+
+NAME[1]=10847
+NAME[2]=12814
+NAME[3]=12878
+NAME[4]=12815
+NAME[5]=12812
+NAME[6]=12813
+NAME[7]=12875
+NAME[8]=12872
+NAME[9]=12873
+NAME[10]=12874
+workdir=$1
+for i in 1 2 3 4 5 6 7 8 9 10
+do 
+projectdir=$workdir/$i
+python ~/HiC-Analysis/mapping_reads_to_loops.py $projectdir/${NAME[$i]}_1k.bed ~/data/
+loops_snps_promoter_list.txt > $projectdir/${NAME[$i]}_1k_in_loops.bed
+wait
+done
