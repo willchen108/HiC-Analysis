461d84eebad04615921a1018e75ed2b090781202
willchen108 <wchen108@uw.edu>
2016-04-27 21:46:58 -0700

    up

---
+++ mapping_qsub.sh
@@ -0,0 +1,4 @@
+. /etc/profile.d/modules.sh
+projectdir = $1
+name = $2
+python ~/HiC-Analysis/mapping_reads_to_loops.py $projectdir/*_1k.bed ~/data/loops_snps_promoter_list.txt > $projectdir/$name-1k_in_loops.bed
