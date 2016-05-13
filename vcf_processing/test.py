e1aebc1fc118fe487f6ab42d86fff691dc62f58c
willchen108 <wchen108@uw.edu>
2016-05-13 15:34:15 -0700

    up

--- vcf_processing/test.py
+++ vcf_processing/test.py
@@ -3,7 +3,7 @@ import csv
 VCFfile = open(sys.argv[1])
 IDs = {}
 for line in VCFfile:
-	ID = line.split()[2]
+	Chrom, Pos, ID, REF, ALT, QUAL, FILTER, INFO, FORMAT, s1, s2, s3, s4, s5, s6, s7, s8, s9 = line.split()[0:18]
 	if ID in IDs: 
 		print 'copy'
 	else:
