1ed701cdb81aedad9879b0a650fc5a5f530197b2
willchen108 <wchen108@uw.edu>
2016-05-13 12:44:47 -0700

    up

--- Contactmap/untitled.sh
+++ Contactmap/untitled.sh
@@ -2,7 +2,7 @@
 #require 2 cores 16G memory.
 #USAGE: contact_map_bulk.sh <workdirectory> example /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/eQTL_SNPs_151228/Promoters/
 #VARS 
-
+module load vcflib/20150313
 NAME[1]=10847
 NAME[2]=12814
 NAME[3]=12878
@@ -13,13 +13,18 @@ NAME[7]=12875
 NAME[8]=12872
 NAME[9]=12873
 NAME[10]=12874
+workdir=$1
 for i in {1..10}
 do 
 cd $i
-samtools sort -o ${NAME[$i]}_merged_RG.sorted.bam ${NAME[$i]}_merged_RG.bam
+java -jar /net/shendure/vol1/home/wchen108/tools/picard-tools-1.141/picard.jar BuildBamIndex \
+	I=${NAME[$i]}_merged_RG.sorted.bam &
 cd ../
 done
 
+
+java -jar /net/shendure/vol1/home/wchen108/tools/picard-tools-1.141/picard.jar BuildBamIndex \
+    I=10847_merged_RG.sorted.bam
 
 NAME[1]=10847
 NAME[2]=12814
@@ -34,11 +39,11 @@ NAME[10]=12874
 for i in {1..10}
 do 
  java -jar /net/gs/vol3/software/modules-sw/GATK/3.5/Linux/RHEL6/x86_64/GenomeAnalysisTK.jar \
-   -R /net/shendure/vol10/nobackup/shared/GATK.resources/v2.5/ucsc.hg19.fasta \
+   -R /net/shendure/vol10/nobackup/shared/genomes/human_g1k_hs37d5/hs37d5.fa \
    -T ASEReadCounter \
    -o /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/promoter_capture_112515/Promoters/$i/${NAME[$i]}_promoter.csv \
    -I /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/promoter_capture_112515/Promoters/$i/${NAME[$i]}_merged_RG.bam \
-   -sites /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/SNPs_release/eQTL_SNPs/All_eQTL_SNPs.vcf \
+   -sites /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/SNPs_release/eQTL_SNPs/samples/${NAME[$i]}_eQTL_SNPs.vcf \
    -U ALLOW_N_CIGAR_READS \ &
 done
 
---
+++ GATK_ASE_bulk.sh
@@ -0,0 +1,27 @@
+#Create by Will Chen @ 2016.05.13
+#requires 10 cores and 5G
+#usage sh GATK_ASE_bulk.sh /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/eQTL_SNPs_151228/Promoters eQTL
+#					or     /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/promoter_capture_112515/Promoters promoters
+NAME[1]=10847
+NAME[2]=12814
+NAME[3]=12878
+NAME[4]=12815
+NAME[5]=12812
+NAME[6]=12813
+NAME[7]=12875
+NAME[8]=12872
+NAME[9]=12873
+NAME[10]=12874
+workdir=$1
+suffix=$2
+for i in {1..10}
+do 
+projectdir=$workdir/$i
+ java -jar /net/gs/vol3/software/modules-sw/GATK/3.5/Linux/RHEL6/x86_64/GenomeAnalysisTK.jar \
+   -R /net/shendure/vol10/nobackup/shared/genomes/human_g1k_hs37d5/hs37d5.fa \
+   -T ASEReadCounter \
+   -o $projectdir/${NAME[$i]}_$suffix.csv \
+   -I $projectdir/${NAME[$i]}_merged_RG.sorted.bam \
+   -sites /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/SNPs_release/eQTL_SNPs/samples/${NAME[$i]}_eQTL_SNPs.vcf \
+   -U ALLOW_N_CIGAR_READS \ &
+done
