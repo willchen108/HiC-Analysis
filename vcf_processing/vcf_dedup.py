42bc458900386426df141a172bd03cc1d68bf5af
willchen108 <wchen108@uw.edu>
2016-05-13 15:39:20 -0700

    push

--- vcf_processing/vcf_dedup.py
+++ vcf_processing/vcf_dedup.py
@@ -1,15 +1,14 @@
 import os,sys,re
 import csv
-
 VCFfile = open(sys.argv[1])
 IDs = {}
 for line in VCFfile:
 	if '#' in line:
 		print line.strip('\n')
 	else:
-		ID = line.split()[2]
+		Chrom, Pos, ID= line.split()[0:3]
 		if ID in IDs: 
-			test =1
+			test = 1
 		else:
 			IDs[ID] = []
 			print line.strip('\n')
