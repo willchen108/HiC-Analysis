714f2cfa85b96ed0f905f2f634813eb669a8a276
willchen108 <wchen108@uw.edu>
2016-05-13 15:36:40 -0700

    push

--- vcf_processing/test.py
+++ vcf_processing/test.py
@@ -3,9 +3,12 @@ import csv
 VCFfile = open(sys.argv[1])
 IDs = {}
 for line in VCFfile:
-	Chrom, Pos, ID, REF, ALT, QUAL, FILTER, INFO, FORMAT, s1, s2, s3, s4, s5, s6, s7, s8, s9 = line.split()[0:18]
-	if ID in IDs: 
-		print 'copy'
-	else:
-		IDs[ID] = []
+	if '#' in line:
 		print line.strip('\n')
+	else:
+		Chrom, Pos, ID= line.split()[0:3]
+		if ID in IDs: 
+			print 'copy'
+		else:
+			IDs[ID] = []
+			print line.strip('\n')
