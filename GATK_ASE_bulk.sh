a225df6b28b938c0d320215afc8e7479290103a5
willchen108 <wchen108@uw.edu>
2016-05-13 12:46:55 -0700

    up

--- GATK_ASE_bulk.sh
+++ GATK_ASE_bulk.sh
@@ -23,5 +23,6 @@ projectdir=$workdir/$i
    -o $projectdir/${NAME[$i]}_$suffix.csv \
    -I $projectdir/${NAME[$i]}_merged_RG.sorted.bam \
    -sites /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/SNPs_release/eQTL_SNPs/samples/${NAME[$i]}_eQTL_SNPs.vcf \
-   -U ALLOW_N_CIGAR_READS \ &
+   -U ALLOW_N_CIGAR_READS \
+   &
 done
