5beba99c25ab048fe12c6fb9a5b20fb38b4c33b8
willchen108 <wchen108@uw.edu>
2016-05-13 16:52:36 -0700

    push

--- Allele_specific_count/SNPsCap_PromoterCap_merge.sh
+++ Allele_specific_count/SNPsCap_PromoterCap_merge.sh
@@ -15,6 +15,6 @@ for i in {1..10}
 do 
 p1=/net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/eQTL_SNPs_151228/Promoters/$i
 p2=/net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/promoter_capture_112515/Promoters/$i
-python ~/HiC-Analysis/Allele_specific_count/SNPsCap_PromoterCap_merge.py $p1 $p2 > /net/shendure/vol1/home/wchen108/data/AScount/${NAME[$i]}_merged_count.csv &
+python ~/HiC-Analysis/Allele_specific_count/SNPsCap_PromoterCap_merge.py $p1/${NAME[$i]}_eQTL.csv $p2/${NAME[$i]}_promoters.csv > /net/shendure/vol1/home/wchen108/data/AScount/${NAME[$i]}_merged_count.csv &
 done
 
