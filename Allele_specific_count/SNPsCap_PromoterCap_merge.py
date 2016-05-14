97e9461d374257277f5948f420685bc2473d6ece
willchen108 <wchen108@uw.edu>
2016-05-13 17:01:31 -0700

    push

--- Allele_specific_count/SNPsCap_PromoterCap_merge.py
+++ Allele_specific_count/SNPsCap_PromoterCap_merge.py
@@ -11,7 +11,8 @@ for line in f1:
         Chrom, Pos, ID, REF, ALT, REFcount, ALTcount, TOTALcount = line.split()[0:8]
         temp[(Chrom,ID)] = [Chrom, Pos, ID, REF, ALT, int(REFcount), int(ALTcount), int(TOTALcount)]
     else:
-    	head = line.strip('\n')
+    	Chrom, Pos, ID, REF, ALT, REFcount, ALTcount, TOTALcount = line.split()[0:8]
+        head = [Chrom, Pos, ID, REF, ALT, REFcount, ALTcount, TOTALcount]
 
 for line in f2:
     if 'contig' not in line:
@@ -22,7 +23,7 @@ for line in f2:
             temp[(Chrom,ID)][7] += int(TOTALcount)
         else:
             temp[(Chrom,ID)] = [Chrom, Pos, ID, REF, ALT, int(REFcount), int(ALTcount), int(TOTALcount)]
-
+    
+print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (head[0],head[1],head[2],head[3],head[4],head[5],head[6],head[7])
 for i in temp:
-	print "%s" % (head)
 	print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (temp[i][0],temp[i][1],temp[i][2],temp[i][3],temp[i][4],temp[i][5],temp[i][6],temp[i][7]) 
