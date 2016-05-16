c6d94fb340c09633ce775321e6062b8cac2c0b8e
willchen108 <wchen108@uw.edu>
2016-05-16 11:40:49 -0700

    push

---
+++ Allele_specific_count/merge_bed.py
@@ -0,0 +1,25 @@
+import os,sys,re
+
+bed_r1 = open(sys.argv[1])
+bed_r2 = open(sys.argv[2])
+
+org = {}
+
+
+for line in bed_r1:
+	chrid,fcoord,rcoord,title,qual,strand = line.split()
+	if int(qual) > 0:
+		if title in org:
+			org[title] += 1
+		else:
+			org[title] = 0
+			org[title] += 1
+
+for line in bed_r2:
+	chrid,fcoord,rcoord,title,qual,strand = line.split()
+	if int(qual) > 0:
+		if title in org:
+			org[title] += 1
+		else:
+			org[title] = 0
+			org[title] += 1
