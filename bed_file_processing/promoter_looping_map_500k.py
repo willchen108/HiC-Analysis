13ec32f1f83ee63b039111297ecb88a7a43c5ee4
willchen108 <wchen108@uw.edu>
2016-05-25 19:37:56 -0700

    push

--- bed_file_processing/promoter_looping_map_500k.py
+++ bed_file_processing/promoter_looping_map_500k.py
@@ -3,17 +3,20 @@
 #promoter is 101th and 500kb upstream is [1-100] 500kb downstream is [102-201]. 0 is reads that are >100kb upstream; 202 is >100kb downstream.
 # python ~/HiC-Analysis/bed_file_processing/promoter_looping_map.py /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/probes/gencode.v19_promoter_chr_removed.bed ${NAME[$i]}_joint_SPloops_1k.dedup.bed > ${NAME[$i]}_SPloops.matrix &
 
-
+import pickle
 import os,sys,re
 #BED file of mapped reads
 bedfile = open(sys.argv[1])
 bed = open(sys.argv[2])
+prefix = sys.argv[2].split('_')[0]
 #Just a list with different types of ligation junctions
 res = 5000
 num = int(500000/res) + 3
 promoters = {}
+prom_list = []
 for line in bedfile:
 	chrom, start, end = line.split()[0:3]
+	prom_list.append([chrom, start, end])
 	if chrom in promoters:
 		promoters[chrom][(start,end)] = [0]*num
 	else:
@@ -60,8 +63,17 @@ for line in bed:
 					else:
 						promoters[species][promoter][0] +=1
 
-for chrom in promoters:
-	for promoter in promoters[chrom]:
-		s = promoters[chrom][promoter]
-		print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s"% \
-    (chrom,promoter[0],promoter[1],s[0],s[1],s[2],s[3],s[4],s[5],s[6],s[7],s[8],s[9],s[10],s[11],s[12],s[13],s[14],s[15],s[16],s[17],s[18],s[19],s[20],s[101],s[22],s[23],s[24],s[25],s[26],s[27],s[28],s[29],s[30],s[31],s[32],s[33],s[34],s[35],s[36],s[37],s[38],s[39],s[40],s[41],s[42])
+fname = prefix + '_joint_SPloops_1k.pickle'
+with open(fname,'wb') as handle:
+    pickle.dump(promoters,handle)
+
+matrix = np.zeros(shape=(len(prom_list),num),dtype=float)
+
+for i in range(len(prom_list)):
+	chrom, start, end = prom_list[i][0], prom_list[i][1], prom_list[i][2]
+	matrix[i,] = promoters[chrom][(start,end)]
+figname = prefix + '_joint_SPloops_1k.pdf'
+plt.figure()
+plt.imshow(matrix,origin="lower",cmap=plt.get_cmap('YlOrRd'),interpolation="nearest",aspect='auto',vmin=0)
+plt.colorbar()
+plt.savefig(figname)
