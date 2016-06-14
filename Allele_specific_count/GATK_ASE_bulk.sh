e6cfa8cc2a631d54e313373a8ae22c8d06899073
willchen108 <wchen108@uw.edu>
2016-06-14 12:02:39 -0700

    push

--- Allele_specific_count/GATK_ASE_bulk.sh
+++ Allele_specific_count/GATK_ASE_bulk.sh
@@ -2,8 +2,8 @@
 #requires 10 cores and 5G
 #usage sh ~/HiC-Analysis/Allele_specific_count/GATK_ASE_bulk.sh /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/eQTL_SNPs_151228/Promoters eQTL_remapped
 # 	or sh ~/HiC-Analysis/Allele_specific_count/GATK_ASE_bulk.sh /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/promoter_capture_112515/Promoters promoters_remapped
-# /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/dhc_paired_bams/WASP snps.wasp.sorted.chr snps_realigned_dedup_wasp
-
+# /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/dhc_paired_bams/WASP snps.wasp.sorted.RG.chr snps_realigned_dedup_wasp_20160613
+# /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/dhc_paired_bams/WASP promoters.wasp.sorted.chr.RG promoters_realigned_dedup_wasp_20160613
 NAME[1]=10847
 NAME[2]=12814
 NAME[3]=12878
@@ -15,16 +15,16 @@ NAME[8]=12872
 NAME[9]=12873
 NAME[10]=12874
 workdir=$1
-suffix_in=$2
-suffix_out=$3
+suffix_I=$2
+suffix_O=$3
 for i in {1..10}
 do 
 projectdir=$workdir/$i
  java -jar /net/gs/vol3/software/modules-sw/GATK/3.5/Linux/RHEL6/x86_64/GenomeAnalysisTK.jar \
    -R /net/shendure/vol10/nobackup/shared/genomes/human_g1k_hs37d5/hs37d5.fa \
    -T ASEReadCounter \
-   -o $projectdir/${NAME[$i]}_$suffix_out.csv \
-   -I $projectdir/${NAME[$i]}_$suffix_in.bam \
+   -o $projectdir/${NAME[$i]}_$suffix_O.csv \
+   -I $projectdir/${NAME[$i]}_$suffix_I.bam \
    -sites /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/SNPs_release/eQTL_SNPs/Biallelic_eQTL_SNPs.vcf \
    -U ALLOW_N_CIGAR_READS \
    &
