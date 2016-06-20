9bb50568f12dd7ed06b1c815fc70a0b239a78a1a
willchen108 <wchen108@uw.edu>
2016-06-20 14:44:30 -0700

    up

--- Allele_specific_count/WASP/WASP_mapping_v2.sh
+++ Allele_specific_count/WASP/WASP_mapping_v2.sh
@@ -10,27 +10,33 @@ cd $projdir
 mkdir -p WASP/$i
 mv ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.bam WASP/$i
 mv ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.bam WASP/$i
-#Step 2 
+
 cd WASP/$i
 
-python ~/tools/WASP/mapping/find_intersecting_snps.py -s ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.bam /net/shendure/vol10/projects/DNaseHiC.eQTLs/data/SNPlist_wasp/ &
-python ~/tools/WASP/mapping/find_intersecting_snps.py -s ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.bam /net/shendure/vol10/projects/DNaseHiC.eQTLs/data/SNPlist_wasp/ &
+# Sort again
+samtools sort -o ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.bam ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.bam &
+samtools sort -o ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.bam ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.bam &
+wait
+
+#Step 2 
+python ~/tools/WASP/mapping/find_intersecting_snps.py -s ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.bam /net/shendure/vol10/projects/DNaseHiC.eQTLs/data/SNPlist_wasp/ &
+python ~/tools/WASP/mapping/find_intersecting_snps.py -s ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.bam /net/shendure/vol10/projects/DNaseHiC.eQTLs/data/SNPlist_wasp/ &
 wait
 
 #Step 3
-bwa mem -M /net/shendure/vol10/nobackup/shared/alignments/bwa-0.6.1/human_g1k_hs37d5/hs37d5.fa ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.remap.fq.gz | samtools view -bS > ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.remapped.bam &
+bwa mem -M /net/shendure/vol10/nobackup/shared/alignments/bwa-0.6.1/human_g1k_hs37d5/hs37d5.fa ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.remap.fq.gz | samtools view -bS > ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.remapped.bam &
 
-bwa mem -M /net/shendure/vol10/nobackup/shared/alignments/bwa-0.6.1/human_g1k_hs37d5/hs37d5.fa ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.remap.fq.gz | samtools view -bS > ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.remapped.bam &
+bwa mem -M /net/shendure/vol10/nobackup/shared/alignments/bwa-0.6.1/human_g1k_hs37d5/hs37d5.fa ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.remap.fq.gz | samtools view -bS > ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.remapped.bam &
 wait
 
 #Step 4
-python ~/tools/WASP/mapping/filter_remapped_reads.py ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.to.remap.bam ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.remapped.bam ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.remap.keep.bam ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.to.remap.num.gz&
+python ~/tools/WASP/mapping/filter_remapped_reads.py ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.to.remap.bam ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.remapped.bam ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.remap.keep.bam ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.to.remap.num.gz&
 
-python ~/tools/WASP/mapping/filter_remapped_reads.py ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.to.remap.bam ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.remapped.bam ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.remap.keep.bam ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.to.remap.num.gz&
+python ~/tools/WASP/mapping/filter_remapped_reads.py ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.to.remap.bam ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.remapped.bam ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.remap.keep.bam ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.to.remap.num.gz&
 wait
 
-samtools merge ${NAME}_promoters.wasp.bam ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.keep.bam ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.remap.keep.bam &
-samtools merge ${NAME}_snps.wasp.bam ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.keep.bam ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.remap.keep.bam &
+samtools merge ${NAME}_promoters.wasp.bam ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.keep.bam ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.remap.keep.bam &
+samtools merge ${NAME}_snps.wasp.bam ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.keep.bam ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.sort.remap.keep.bam &
 wait
 
 #Sort for RNA-seq data
@@ -43,8 +49,8 @@ samtools sort -o $projdir/WASP/$i/${NAME}_promoters.wasp.sorted.chr.bam $projdir
 samtools sort -o $projdir/WASP/$i/${NAME}_snps.wasp.sorted.chr.bam $projdir/WASP/$i/${NAME}_snps.wasp.bam&
 wait
 
-samtools index ${NAME}_promoters.wasp.sorted.chr.bam
-samtools index ${NAME}_snps.wasp.sorted.chr.bam
-
+samtools index ${NAME}_promoters.wasp.sorted.chr.bam &
+samtools index ${NAME}_snps.wasp.sorted.chr.bam &
+wait
 mv ${NAME}_promoters.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.bam ../../
 mv ${NAME}_snps.fix.secondary_alignments_flagged_coordinate_sorted_dups_removed.bam ../../
