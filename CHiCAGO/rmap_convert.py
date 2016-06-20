9a2aebb7db8dc58b79936b3f4a71fb2b34f0dfcd
willchen108 <wchen108@uw.edu>
2016-06-20 11:21:17 -0700

    up

--- CHiCAGO/rmap_convert.py
+++ CHiCAGO/rmap_convert.py
@@ -2,8 +2,6 @@
 # python /net/shendure/vol1/home/wchen108/HiC-Analysis/CHiCAGO/rmap_convert.py > /net/shendure/vol10/projects/DNaseHiC.eQTLs/data/Dnasemap_hs37d5.rmap
 import os,sys,re
 rmap = open('/net/shendure/vol10/projects/DNaseHiC.eQTLs/data/Dnase_map_hs37d5.rmap')
-print "%s\t%s\t%s\t%s" % \
-    ('chr', 'start', 'end', 'fragmentID')
 for line in rmap:
     if "start" not in line:
         if 'dna:' in line:
