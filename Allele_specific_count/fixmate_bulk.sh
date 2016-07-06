df4168b44a990d27eb0a5efda7c5a2b13e1a7c1f
willchen108 <wchen108@uw.edu>
2016-07-05 22:42:51 -0700

    push

--- Allele_specific_count/fixmate_bulk.sh
+++ Allele_specific_count/fixmate_bulk.sh
@@ -1,7 +1,7 @@
 #Create by Will Chen @ 2016.06.16
 #requires 10 cores and 10G
-#usage sh ~/HiC-Analysis/Allele_specific_count/fixmate_bulk.sh.sh /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/eQTL_SNPs_151228/Promoters snps_properPairs_fixmate /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/dhcpair_Will
-#	   sh ~/HiC-Analysis/Allele_specific_count/fixmate_bulk.sh.sh /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/promoter_capture_112515/Promoters promoters_properPairs_fixmate /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/dhcpair_Will
+#usage sh ~/HiC-Analysis/Allele_specific_count/fixmate_bulk.sh /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/eQTL_SNPs_151228/Promoters snps_properPairs_fixmate /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/dhcpair_Will
+#	   sh ~/HiC-Analysis/Allele_specific_count/fixmate_bulk.sh /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/promoter_capture_112515/Promoters promoters_properPairs_fixmate /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/dhcpair_Will
 
 workdir=$1
 suffix=$2
@@ -9,4 +9,4 @@ destdir=$3
 for i in {1..10}
 do 
 sh ~/HiC-Analysis/Allele_specific_count/fixmate_pipeline.sh $workdir/$i $i $suffix $destdir/$i &
-done
+done 
