68db46a329712823d6d02750394dd4059892dda5
willchen108 <wchen108@uw.edu>
2016-07-05 21:57:11 -0700

    push

--- Allele_specific_count/fixmate_pipeline.sh
+++ Allele_specific_count/fixmate_pipeline.sh
@@ -23,15 +23,19 @@ i=$2
 suffix=$3
 destdir=$4
 
+# Sort first
+samtools sort -@ 5 -o $workdir/${NAME[$i]}_S${i}_R1_001.fastq.bwam.sort.bam $workdir/${NAME[$i]}_S${i}_R1_001.fastq.bwam.bam &
+samtools sort -@ 5 -o $workdir/${NAME[$i]}_S${i}_R2_001.fastq.bwam.sort.bam $workdir/${NAME[$i]}_S${i}_R2_001.fastq.bwam.bam &
+wait
 # WASP
-sh ~/HiC-Analysis/Allele_specific_count/WASP/WASP_mapping.sh $workdir ${NAME[$i]} S${i}_R1_001.fastq.bwam &
-sh ~/HiC-Analysis/Allele_specific_count/WASP/WASP_mapping.sh $workdir ${NAME[$i]} S${i}_R2_001.fastq.bwam &
+sh ~/HiC-Analysis/Allele_specific_count/WASP/WASP_mapping.sh $workdir ${NAME[$i]} S${i}_R1_001.fastq.bwam.sort &
+sh ~/HiC-Analysis/Allele_specific_count/WASP/WASP_mapping.sh $workdir ${NAME[$i]} S${i}_R2_001.fastq.bwam.sort &
 wait
 
 # Merge 2 bam files and add pair flag
-r1=$workdir/${NAME[$i]}_S${i}_R1_001.fastq.bwam.bam
-r2=$workdir/${NAME[$i]}_S${i}_R2_001.fastq.bwam.bam 
-( ~mkircher/bin/samtools view -H $r1; ~mkircher/bin/samtools view -X $r1 | awk 'BEGIN{ FS="\t"; OFS="\t";}{ $2="pP1"$2; print }' ; ~mkircher/bin/samtools view -X $r2 | awk 'BEGIN{ FS="\t"; OFS="\t";}{ $2="pP2"$2; print }' ) | ~mkircher/bin/samtools view -Su - | samtools sort -n -@ 10 - -T $workdir/${NAME[$i]}_test_snps | samtools fixmate -r - $destdir/${NAME[$i]}_${suffix}.bam
+r1=$workdir/${NAME[$i]}_S${i}_R1_001.fastq.bwam.sort.wasp.bam
+r2=$workdir/${NAME[$i]}_S${i}_R2_001.fastq.bwam.sort.wasp.bam 
+( ~mkircher/bin/samtools view -H $r1; ~mkircher/bin/samtools view -X $r1 | awk 'BEGIN{ FS="\t"; OFS="\t";}{ $2="pP1"$2; print }' ; ~mkircher/bin/samtools view -X $r2 | awk 'BEGIN{ FS="\t"; OFS="\t";}{ $2="pP2"$2; print }' ) | ~mkircher/bin/samtools view -Su - | samtools sort -n -@ 10 - -T $workdir/${NAME[$i]}_test_snps | samtools fixmate -r -p - $destdir/${NAME[$i]}_${suffix}.bam
 # sort
 samtools sort -@ 10 -o $destdir/${NAME[$i]}_${suffix}.sorted.bam $destdir/${NAME[$i]}_${suffix}.bam
 
