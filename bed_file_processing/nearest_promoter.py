baf7aa4693b537041a3f0e954f22e1e6e0e37ce6
willchen108 <wchen108@uw.edu>
2016-08-23 09:51:01 -0700

    push

--- bed_file_processing/nearest_promoter.py
+++ bed_file_processing/nearest_promoter.py
@@ -1,6 +1,6 @@
 #Create by Will Chen @ 2016.08.22
 # This file is used create a table that shows the nearest promoter to a SNP
-# 
+# usage: python /net/shendure/vol1/home/wchen108/HiC-Analysis/bed_file_processing/nearest_promoter.py /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/probes/eqtl_capture_just_all_eqtls_all_promoter_snps_excluded_snp_coords_centered_on_snp_merged_no_promoter_snps.chr_removed.bed /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/probes/gencode.v19_promoter_chr_removed.bed > /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/SNP_nearest_promoter_list.bed
 
 import os,sys,re
 #BED file of mapped reads
@@ -35,4 +35,4 @@ for chrom in SNPs:
 			if abs(dis) < abs(mdis):
 				mdis = dis
 				pro  = promoter
-		print "%s\t%s\t%s\t%s\t%s"% (chrom,snp[0],snp[1],pro[0],pro[1],pro[2])
+		print "%s\t%s\t%s\t%s\t%s"% (chrom,snp[0]-50,snp[0]+50,snp[1],pro[0],pro[1],pro[2])
