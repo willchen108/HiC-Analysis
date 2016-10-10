f04298c6825e96c74a95c12aca95ad358ab72fe6
willchen108 <wchen108@uw.edu>
2016-10-10 10:51:12 -0700

    push

--- Allele_specific_count/fixmate_pipeline.sh
+++ Allele_specific_count/fixmate_pipeline.sh
@@ -62,39 +62,41 @@ java -jar /net/shendure/vol1/home/wchen108/tools/picard-tools-1.141/picard.jar M
 
 #Add Readgroup
 java -jar /net/shendure/vol1/home/wchen108/tools/picard-tools-1.141/picard.jar AddOrReplaceReadGroups \
-I=$destdir/${NAME[$i]}.${suffix}.sorted.dedup.bam \
-O=$destdir/${NAME[$i]}.${suffix}.sorted.dedup.RG.bam  \
-RGID=NA${NAME[$i]} \
-RGLB=lib1 \
-RGPL=illumina \
-RGPU=unit1 \
-RGSM=20 \
-VALIDATION_STRINGENCY=SILENT
-
-
-# Sort again
-samtools sort -@ 10 -o $destdir/${NAME[$i]}.${suffix}.sorted.dedup.sort.bam $destdir/${NAME[$i]}.${suffix}.sorted.dedup.bam
-samtools index $destdir/${NAME[$i]}.${suffix}.sorted.dedup.sort.RG.bam
+      I=$destdir/${NAME[$i]}.${suffix}.sorted.dedup.bam \
+      O=$destdir/${NAME[$i]}.${suffix}.sorted.dedup.RG.bam  \
+      RGID=NA${NAME[$i]} \
+      RGLB=lib1 \
+      RGPL=illumina \
+      RGPU=unit1 \
+      RGSM=20 \
+      VALIDATION_STRINGENCY=SILENT
 
 # sortname
 samtools sort -@ 10 -n -o $destdir/${NAME[$i]}.${suffix}.sorted.dedup.RG.sortname.bam $destdir/${NAME[$i]}.${suffix}.sorted.dedup.RG.bam
-bedtools bamtobed -bedpe -mate1 -i $destdir/${NAME[$i]}_${suffix}.sorted.dedup.sort.RG.bam > $destdir/${NAME[$i]}_${suffix}.bedpe
+
+#convert to paired bed files
+bedtools bamtobed -bedpe -mate1 -i $destdir/${NAME[$i]}.${suffix}.sorted.dedup.RG.sortname.bam > $destdir/${NAME[$i]}.${suffix}.temp.bedpe
 wait
+python /net/shendure/vol10/projects/DNaseHiC.eQTLs/scripts/dedupe_bed.py $destdir/${NAME[$i]}.${suffix}.temp.bedpe > $destdir/${NAME[$i]}.${suffix}.bedpe
 rm $destdir/${NAME[$i]}.${suffix}.sorted.dedup.bam
 rm $destdir/${NAME[$i]}.${suffix}.sorted.bam
+rm $destdir/${NAME[$i]}.${suffix}.temp.bedpe
+
+# subset intra 3k, 10k and intra pairs
+python /net/shendure/vol1/home/wchen108/HiC-Analysis/bed_file_processing/bed_partition.py intra 3000 $destdir/${NAME[$i]}.${suffix}.bedpe > $destdir/${NAME[$i]}.$suffix.intra3k.bed &
+python /net/shendure/vol1/home/wchen108/HiC-Analysis/bed_file_processing/bed_partition.py intra 10000 $destdir/${NAME[$i]}.${suffix}.bedpe > $destdir/${NAME[$i]}.$suffix.intra10k.bed &
+python /net/shendure/vol1/home/wchen108/HiC-Analysis/bed_file_processing/bed_partition.py inter 0 $destdir/${NAME[$i]}.${suffix}.bedpe > $destdir/${NAME[$i]}.$suffix.inter.bed &
+wait
+# Subset SPloops
+python /net/shendure/vol1/home/wchen108/HiC-Analysis/bed_file_processing/bed_subset_SPloop.py /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/probes/gencode.v19_promoter_chr_removed.bed /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/probes/eqtl_snps_centered_snp_101bp_chr_removed.bed $destdir/${NAME[$i]}.$suffix.intra3k.bed > $destdir/${NAME[$i]}.$suffix.intra3k.SPloop.bed &
+python /net/shendure/vol1/home/wchen108/HiC-Analysis/bed_file_processing/bed_subset_SPloop.py /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/probes/gencode.v19_promoter_chr_removed.bed /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/probes/eqtl_snps_centered_snp_101bp_chr_removed.bed $destdir/${NAME[$i]}.$suffix.intra10k.bed > $destdir/${NAME[$i]}.$suffix.intra10k.SPloop.bed &
+wait
 #convert to h5 files
 #Rscript ~/HiC-Analysis/Plotting/prepPseudoPairs.r $destdir/ ${NAME[$i]} ${suffix}.sorted.dedup.sort.RG &
 
-#convert to paired bed files
-
-
-# subset intra 3k and 10k pairs
-python ~/HiC-Analysis/bed_file_processing/bed_partition.py 3000 $destdir/${NAME[$i]}_${suffix}.bedpe > $destdir/${NAME[$i]}.$suffix.intra3k.bed &
-python ~/HiC-Analysis/bed_file_processing/bed_partition.py 10000 $destdir/${NAME[$i]}_${suffix}.bedpe > $destdir/${NAME[$i]}.$suffix.intra10k.bed &
-
-# Subset SPloops
-python ~/HiC-Analysis/bed_file_processing/bed_subset_SPloop.py /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/probes/gencode.v19_promoter_chr_removed.bed $destdir/${NAME[$i]}.$suffix.intra3k.bed > $destdir/${NAME[$i]}.$suffix.intra3k.SPloop.bed &ß
-python ~/HiC-Analysis/bed_file_processing/bed_subset_SPloop.py /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/probes/gencode.v19_promoter_chr_removed.bed $destdir/${NAME[$i]}.$suffix.intra10k.bed > $destdir/${NAME[$i]}.$suffix.intra10k.SPloop.bed &
+# Sort again
+samtools sort -@ 10 -o $destdir/${NAME[$i]}.${suffix}.sorted.dedup.RG.sort.bam $destdir/${NAME[$i]}.${suffix}.sorted.dedup.RG.bam
+samtools index $destdir/${NAME[$i]}.${suffix}.sorted.dedup.RG.sort.bam
 
 
 #GATK ASE count
@@ -102,7 +104,7 @@ java -jar /net/gs/vol3/software/modules-sw/GATK/3.5/Linux/RHEL6/x86_64/GenomeAna
   -R /net/shendure/vol10/nobackup/shared/genomes/human_g1k_hs37d5/hs37d5.fa \
   -T ASEReadCounter \
   -o $destdir/${NAME[$i]}.${suffix}.csv \
-  -I $destdir/${NAME[$i]}.${suffix}.sorted.dedup.sort.RG.bam \
+  -I $destdir/${NAME[$i]}.${suffix}.sorted.dedup.RG.sort.bam \
   -sites /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/SNPs_release/eQTL_SNPs/Biallelic_eQTL_SNPs.vcf \
   -U ALLOW_N_CIGAR_READS \
   &
