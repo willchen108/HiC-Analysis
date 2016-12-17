1243df2a22cfa5563eb9c1206a43bc44e6efa74a
willchen108 <wchen108@uw.edu>
2016-12-16 16:04:12 -0800

    push

--- CHiCAGO/bam2chicago_bulk.sh
+++ CHiCAGO/bam2chicago_bulk.sh
@@ -1,7 +1,7 @@
 # This file is create by Will @ 2016.06.17
 # This file is used to convert bam files into chicagoinput
-# Usage sh ~/HiC-Analysis/CHiCAGO/bam2chicago_bulk.sh /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/dhc_paired_bams/ promoters /net/shendure/vol10/projects/DNaseHiC.eQTLs/data/promoter.baitmap
-#    	sh ~/HiC-Analysis/CHiCAGO/bam2chicago_bulk.sh /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/dhc_paired_bams/ snps /net/shendure/vol10/projects/DNaseHiC.eQTLs/data/SNPs.baitmap
+# Usage sh ~/HiC-Analysis/CHiCAGO/bam2chicago_bulk.sh /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/dhc_v2 promoter /net/shendure/vol10/projects/DNaseHiC.eQTLs/data/pro_design/promoter.baitmap
+#    	sh ~/HiC-Analysis/CHiCAGO/bam2chicago_bulk.sh /net/shendure/vol10/projects/DNaseHiC.eQTLs/nobackup/dhc_v2 snps /net/shendure/vol10/projects/DNaseHiC.eQTLs/data/snp_design/SNPs.baitmap
 NAME[1]=10847
 NAME[2]=12814
 NAME[3]=12878
@@ -13,9 +13,9 @@ NAME[8]=12872
 NAME[9]=12873
 NAME[10]=12874
 projectdir=$1
-suffix_I=$2
+suffix=$2
 baitmap=$3
 for i in {1..10}
 do
-qsub -l mfree=5G /net/shendure/vol1/home/wchen108/tools/chicago/chicagoTools/bam2chicago.sh $projectdir/$i/${NAME[$i]}.$suffix_I.bam $baitmap /net/shendure/vol10/projects/DNaseHiC.eQTLs/data/Dnasemap_hs37d5.rmap $projectdir/$i/Chicago/${NAME[$i]}.$suffix_I &
+qsub -l mfree=5G /net/shendure/vol1/home/wchen108/tools/chicago/chicagoTools/bam2chicago.sh $projectdir/$i/${NAME[$i]}.$suffix.fixmate.sorted.dedup.RG.sortname.bam $baitmap /net/shendure/vol10/projects/DNaseHiC.eQTLs/data/Dnasemap_hs37d5.rmap $projectdir/$i/Chicago/${NAME[$i]}.$suffix &
 done
